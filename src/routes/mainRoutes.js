840a58a48a0b157dea1caedcf4322a492c7d199a
AraceliSessolo <araceli.sessolo@gmail.com>
2022-06-27 16:20:15 -0300

    Sprint 5 Agregado de vistas: Quienes somos y Sitio en construcción

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const mainController = require('../controller/mainController');
 
-router.get('/', mainController.index); 
+router.get('/', mainController.index);
+router.get('/quienesSomos', mainController.quienesSomos);
+router.get('/sitioEnConstruccion', mainController.enConstruccion);
 
 module.exports =  router;
