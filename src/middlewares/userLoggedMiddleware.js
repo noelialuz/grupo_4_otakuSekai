16175e32744d14e0e467419387fcb7ad66c3921c
Renzo Damian Aquino <renzo_aquino_1993@hotmail.com>
2022-08-04 19:46:40 -0300

    Renzo Aquino: cambiando nombre e icono del nav

--- src/middlewares/userLoggedMiddleware.js
+++ src/middlewares/userLoggedMiddleware.js
@@ -5,6 +5,11 @@ const { Op } = require("sequelize");
 function userLoggedMiddleware(req, res, next) {
   res.locals.isLogged = false;
 
+ /*let emailInCookie = req.cookies.userEmail
+  console.log(emailInCookie)*/
+
+
+
   if (req.session.usuario) {
     res.locals.isLogged = true;
     res.locals.usuario = req.session.usuario
