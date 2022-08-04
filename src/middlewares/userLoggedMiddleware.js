e509424c609f594ba802ed00e0db5915a6a0cfb4
Renzo Damian Aquino <renzo_aquino_1993@hotmail.com>
2022-08-04 18:18:00 -0300

    Renzo Aquino: agregando nombre e imagen del usuario en la nav

--- src/middlewares/userLoggedMiddleware.js
+++ src/middlewares/userLoggedMiddleware.js
@@ -2,6 +2,7 @@ function userLoggedMiddleware(req, res, next) {
   res.locals.isLogged = false;
   if (req.session.usuario) {
     res.locals.isLogged = true;
+    res.locals.usuario = req.session.usuario
   }
 
   next();
