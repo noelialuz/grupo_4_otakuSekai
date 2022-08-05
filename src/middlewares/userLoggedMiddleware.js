1dbac28222248e04a595b68aa31649d64f9146aa
Renzo Damian Aquino <renzo_aquino_1993@hotmail.com>
2022-08-04 23:21:35 -0300

    corrigiendo errores

--- src/middlewares/userLoggedMiddleware.js
+++ src/middlewares/userLoggedMiddleware.js
@@ -2,12 +2,9 @@ const db = require('../database/models');
 const sequelize = db.sequelize;
 const { Op } = require("sequelize");
 
+
 function userLoggedMiddleware(req, res, next) {
   res.locals.isLogged = false;
-
- /*let emailInCookie = req.cookies.userEmail
-  console.log(emailInCookie)*/
-
 
 
   if (req.session.usuario) {
