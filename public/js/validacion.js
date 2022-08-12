a9fc38a51a652a957632fd1c62ee4ffaa7d02f99
Renzo Damian Aquino <renzo_aquino_1993@hotmail.com>
2022-08-12 17:50:18 -0300

    renzo aquino : agregada la validacion para creacion de productos faltan las extenciones

---
+++ public/js/validacion.js
@@ -0,0 +1,29 @@
+window.addEventListener("load", function () {
+  const extensionesValidas = ".png, .gif, .jpeg, .jpg";
+  let errores = []
+  let formulario = document.querySelector("form.create-form");
+
+  formulario.addEventListener("submit", (e) => {
+    
+    let nombreProducto = document.querySelector("input.nombreProducto");
+    if (nombreProducto.value == "") {
+      errores.push("El Campo de Nombre tiene que estar completo");
+    } else if (nombreProducto.value.length < 5) {
+        errores.push("El Campo de Nombre tiene que tener mas de 5 caracteres");
+    }
+    let descripcion = document.querySelector("input.descripcionProducto");
+    if (descripcion.value == "") {
+        errores.push("El Campo de descripcion tiene que estar completo");
+    } else if (descripcion.value.length < 20) {
+        errores.push("El Campo de descripcion tiene que tener mas de 20 caracteres");
+    }
+    if (errores.length > 0){
+        e.preventDefault();
+        let ulErrores = document.querySelector("div.erroresValidacion ul");
+        for (let i = 0; i < errores.length; i++) {
+           
+            ulErrores.innerHTML += "<li>" +errores[i] + "</li>"
+        }
+    }
+  });
+});
