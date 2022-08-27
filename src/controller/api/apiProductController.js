3cf4f73b0f46a6a89fffb61f34285e91820375c8
BrunoMaiorano <brunomaiorano@gmail.com>
2022-08-27 11:48:05 -0300

    Arreglos en Api Products

--- src/controller/api/apiProductController.js
+++ src/controller/api/apiProductController.js
@@ -5,99 +5,78 @@ const { Op } = require("sequelize");
 
 const dbProducts = db.Products;
 
-
 const productsController = {
-    //funciones de la api Productos
+  //funciones de la api Productos
   list: (req, res) => {
     db.Products.findAll().then(function (allProducts) {
-        db.Categories.findAll().then(function (allCategories){
+      db.Categories.findAll().then(function (allCategories) {
+        let countByCategory = {};
 
-            let totales = [0, 0, 0, 0, 0, 0, 0]
-            
-        for (let x = 0; x < allProducts.length; x++) {
-            ID = allProducts[x].category_id
-            switch (ID) {
-                case 1:
-                    totales[0] = (totales[0] + 1) 
-                    break;
-                case 2:
-                    totales[1] = (totales[1] + 1) 
-                    break;
-                case 3:
-                    totales[2] = (totales[2] + 1) 
-                    break;
-                case 4:
-                    totales[3] = (totales[3] + 1) 
-                    break;
-                case 5:
-                    totales[4] = (totales[4] + 1)    
-                    break;
-                case 6:
-                    totales[5] = (totales[5] + 1)    
-                    break;
-                case 7:
-                    totales[6] = (totales[6] + 1)    
-                    break;
-                default:
-                    break;
+        for (let i = 0; i < allCategories.length; i++) {
+          let contador = 0;
+
+          for (let x = 0; x < allProducts.length; x++) {
+            if (allProducts[x].category_id == allCategories[i].id) {
+              contador = contador + 1;
             }
+          }
+          countByCategory[allCategories[i].description] = contador;
         }
 
-            return res.status(200).json({
-                meta: {
-                    count: allProducts.length,
-                    status: 200,
-                    url: 'api/products'
-                },
-                countByCategory: {
-                Mangas: totales[0],
-                Indumentaria: totales[1],
-                Posters: totales[2],
-                Figuras: totales[3],
-                Tazas: totales[4],
-                Llaveros: totales[5],
-                Otros: totales[6]
-            },
-                data: allProducts
-              })
+        let products = [];
+        let product = {};
 
-        })
-    })
+        for (let b = 0; b < allProducts.length; b++) {
+          product = {
+            id: allProducts[b].id,
+            name: allProducts[b].name,
+            description: allProducts[b].description,
+            relations: ["Categories", "Series"],
+            detail: "api/products/" + allProducts[b].id,
+          };
+          products.push(product);
+        }
+        return res.status(200).json({
+          meta: {
+            count: allProducts.length,
+            status: 200,
+            url: "api/products",
+          },
+          countByCategory: countByCategory,
+          data: products,
+        });
+      });
+    });
   },
-
 
   show: (req, res) => {
     db.Products.findByPk(req.params.id).then(function (product) {
       return res.status(200).json({
-        meta:{
-            status: 200,
-            url: 'api/products/:id'
+        meta: {
+          status: 200,
+          url: "api/products/:id",
         },
         data: product,
-      })
-    })
+      });
+    });
   },
   add: (req, res) => {
-    db.Products
-    .create(req.body)
-    .then(function (product){
+    db.Products.create(req.body).then(function (product) {
       return res.status(200).json({
         data: product,
         status: 200,
-        created: 'true'
-      })
-    })
+        created: "true",
+      });
+    });
   },
 
   delete: (req, res) => {
-    db.Products
-    .destroy({
-      where: {id: req.params.id}
-    })
-    .then(respose => {
-      return res.json(respose)
-    })
+    db.Products.destroy({
+      where: { id: req.params.id },
+    }).then((respose) => {
+      return res.json(respose);
+    });
   },
-}
+};
 
 module.exports = productsController;
